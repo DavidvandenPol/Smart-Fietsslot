63b1b433a3aa55cdc07fcc47168eebf19109d7bc
David van den Pol <david.vanden.pol@gmail.com>
2023-09-11 15:18:30 +0200

    First commit

---
+++ main.py
@@ -0,0 +1,65 @@
+from sense_hat import SenseHat
+import time
+
+sense = SenseHat()
+
+# Drempelwaarden voor versnelling en snelheid van verandering
+acceleration_threshold = 1
+speed_threshold = 2
+
+prev_acceleration = None
+speed_notification_time = None
+knipper_interval = 0.5  # Interval van 0,5 seconde (2 keer per seconde)
+knipper_duration = 10  # Knipperen gedurende 10 seconden
+
+# Kleuren voor het sloticoontje
+r = (255, 0, 0)  # Rood
+b = (0, 0, 0)    # Zwart (uit)
+
+# Het sloticoontje als een lijst van pixels
+lock_icon = [
+    b, b, b, r, r, b, b, b,
+    b, b, r, b, b, r, b, b,
+    b, b, r, b, b, r, b, b,
+    b, b, r, b, b, r, b, b,
+    b, r, r, r, r, r, r, b,
+    b, r, r, r, r, r, r, b,
+    b, r, r, r, r, r, r, b,
+    b, r, r, r, r, r, r, b
+]
+
+while True:
+    acceleration = sense.get_accelerometer_raw()
+    x = acceleration['x']
+    y = acceleration['y']
+    z = acceleration['z']
+
+    x = abs(x)
+    y = abs(y)
+    z = abs(z)
+
+    if prev_acceleration is not None:
+        delta_x = abs(x - prev_acceleration['x'])
+        delta_y = abs(y - prev_acceleration['y'])
+        delta_z = abs(z - prev_acceleration['z'])
+        
+        if delta_x > speed_threshold or delta_y > speed_threshold or delta_z > speed_threshold:
+            if speed_notification_time is None:
+                speed_notification_time = time.time()
+            print("Snelheidsmelding: Versnelling veranderde snel!")
+
+        if speed_notification_time is not None:
+            # Schakel het sloticoontje uit na 10 seconden
+            if time.time() - speed_notification_time >= knipper_duration:
+                speed_notification_time = None
+                sense.set_pixels(lock_icon)  # Laat het sloticoontje zien
+            # Laat het sloticoontje knipperen tijdens de 10 seconden
+            elif (time.time() - speed_notification_time) % (2 * knipper_interval) < knipper_interval:
+                sense.clear()
+            else:
+                sense.set_pixels(lock_icon)  # Laat het sloticoontje zien
+
+    prev_acceleration = acceleration
+    
+    # Print de acceleratie in de terminal
+    print(f"Acceleratie - X: {x:.2f}, Y: {y:.2f}, Z: {z:.2f}")
