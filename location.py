f8e6a50c09b3b1b25fb64984d17db8208d5b004a
GerbenVDH <144111280+GerbenVDH@users.noreply.github.com>
2023-09-12 11:31:22 +0200

    Update location.py

--- location.py
+++ location.py
@@ -7,6 +7,7 @@ import getopt
 verbose = True
 interval = 1
 
+
 gpsd = gps(mode=WATCH_ENABLE|WATCH_NEWSTYLE) 
 
 dbconfig = {
@@ -17,30 +18,45 @@ dbconfig = {
 }
 
 while True:
-    mariadb_connection = mariadb.connect(**dbconfig)
-    if verbose:
-        print("Database connected")
-    cursor = mariadb_connection.cursor()
-    print("Application started!")
+    
     report = gpsd.next()
-
+    
     if report['class'] == 'TPV':
-        longitude = getattr(report,'lat',0.0),"\t",
-        latitude = getattr(report,'lon',0.0),"\t",
-
-        str_lat = str(latitude)
-        str_long = str(longitude)
-    
-        LocationData = str_lat + ', ' + str_long
         
-        cursor.execute("INSERT INTO locatiedata (data) VALUES", (LocationData))
-    
-        mariadb_connection.commit()
-
+        # instantiate a database connection
+        mariadb_connection = mariadb.connect(**dbconfig)
         if verbose:
-            print("Locatie committed")
+            print("Database connected")
+        
+        # create the database cursor for executing SQL queries
+        cursor = mariadb_connection.cursor()
+        
+        report = gpsd.next()
+       
+        longitude_values = getattr(report,'lat',0.0)
+        latitude_values = getattr(report,'lon',0.0)
+         
+        
+        
+        if longitude_values and latitude_values != 0.0:
+            # store measurement in database
+            print(longitude_values)
+            print(latitude_values)
+            
+            insert_query = "INSERT INTO gps_locations (longitude, latitude) VALUES (%s, %s)"
+            cursor.execute(insert_query, (longitude_values, latitude_values))
+        
+        
+            # commit measurements
+            mariadb_connection.commit()
 
+            if verbose:
+                print("Locatie committed")
+
+        # close db connection
         cursor.close()
         mariadb_connection.close()
         
     time.sleep(interval)
+
+
