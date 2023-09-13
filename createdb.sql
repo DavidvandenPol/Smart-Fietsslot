5facd7751c06d43e40054467bfaa037464d1fec6
Gerben van den Hoek <s1196144@student.windesheim.nl>
2023-09-13 14:12:06 +0200

    joo he

--- createdb.sql
+++ createdb.sql
@@ -10,10 +10,11 @@ CREATE TABLE gps_locations (
     latitude DECIMAL(8, 6) DEFAULT NULL
 );
 CREATE TABLE gyro_notifications (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    created_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    islocked boolean not null default 0
 );
+
 CREATE USER 'sensem'@'localhost' IDENTIFIED BY 'h@';
 GRANT INSERT ON smartfiets.gps_locations TO 'sensem'@'localhost';
+GRANT INSERT ON smartfiets.gyro_notifications TO 'sensem'@'localhost';
 GRANT SELECT ON smartfiets.* TO 'sensem'@'localhost';
 GRANT DELETE ON smartfiets.* TO 'sensem'@'localhost';
