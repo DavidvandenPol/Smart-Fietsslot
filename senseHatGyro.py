237e32b91f9dc582638ae25568fc682e0d11d60d
Rick van den Akker <s1193906@student.windesheim.nl>
2023-09-13 11:51:22 +0200

    gyrodb wip

--- senseHatGyro.py
+++ senseHatGyro.py
@@ -2,7 +2,12 @@ from sense_hat import SenseHat
 import time
 import MySQLdb as mariadb
 
-
+dbconfig = {
+    'user': 'sensem',
+    'password': 'h@',
+    'host': 'localhost',
+    'database': 'smartfiets'
+}
 
 sense = SenseHat()
 
@@ -75,10 +80,18 @@ while True:
                 speedNotificationTime = None
                 sense.set_pixels(openLockIcon)
                 print("Meten gestopt.")
+                
+                mariadb_connection.close()
+                print("DB disconnected")
             else:
                 measuring = True
                 sense.set_pixels(openLockIcon)
                 print("Meten gestart.")
+                
+                mariadb_connection = mariadb.connect(**dbconfig)
+                cursor = mariadb_connection.cursor()
+                
+                print("DB connected")
 
     if measuring:
         acceleration = sense.get_accelerometer_raw()
@@ -101,7 +114,14 @@ while True:
                 if speedNotificationTime is None:
                     speedNotificationTime = time.time()
                     showRedLock = True
+                    #print to DB
+                    insert_query = "INSERT INTO gyro_notifications (notification ) VALUES (%s)"
+                    cursor.execute(insert_query,("snelle beweging!"))
+                    
+                    mariadb_connection.commit()                    
+                    
                 print("Verschillingsmelding: Grote verandering in versnelling!")
+                
 
             if speedNotificationTime is not None:
                 if time.time() - speedNotificationTime >= blinkDuration:
