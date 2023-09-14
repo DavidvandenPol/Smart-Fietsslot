9891ba25f6a7250b07426ec588e409b58eccc754
David van den Pol <david.vanden.pol@gmail.com>
2023-09-14 11:10:09 +0200

    IMP: CRAZY SHIT 🚀

--- printdata.py
+++ printdata.py
@@ -10,35 +10,42 @@ mariadb_connection = mariadb.connect(
 
 cursor = mariadb_connection.cursor()
 
+# Query voor het ophalen van GPS-locatiegegevens
 stmt = "SELECT latitude, longitude FROM gps_locations ORDER BY created_date DESC LIMIT 1"
 cursor.execute(stmt)
 row = cursor.fetchone()
 
+# JSON-structuur voor GPS-locatie
 output_json = {
-    "long":  float(row[0]),
-    "lat": float(row[1])
+    "location": {
+        "long":  float(row[1]),
+        "lat": float(row[0])
+    },
+    "islocked": 0,  # Voeg hier de vergrendelingsstatus in als dat van toepassing is
+    "notification": 0  # Voeg hier de meldingsstatus in als dat van toepassing is
 }
 
+# Query voor het ophalen van vergrendelingsstatus
 stmt_2 = "SELECT islocked FROM gyro_status;"
 cursor.execute(stmt_2)
 row = cursor.fetchone()
 
-output_json_2 = {
-    "islocked": round(float(row[0]))
-}
+# Voeg de vergrendelingsstatus toe aan de JSON-structuur
+output_json["islocked"] = round(float(row[0]))
 
+# Query voor het ophalen van meldingsstatus
 stmt_3 = "SELECT notification FROM gyro_notifications;"
 cursor.execute(stmt_3)
 row = cursor.fetchone()
 
-output_json_3 = {
-    "notification": round(float(row[0]))
-}
+# Voeg de meldingsstatus toe aan de JSON-structuur
+output_json["notification"] = round(float(row[0]))
 
 cursor.close()
 mariadb_connection.close()
 
-combined_json = [output_json, output_json_2, output_json_3]
+# Plaats de JSON-structuur in een lijst om hetzelfde formaat te behouden als eerder
+combined_json = [output_json]
 
 print("Content-type: application/json\n")
 print(json.dumps(combined_json))
