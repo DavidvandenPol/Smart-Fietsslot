a8383676c54476cdca08dfb3f551aea9c2080dc0
Gerben van den Hoek <s1196144@student.windesheim.nl>
2023-09-14 10:31:07 +0200

    hallo

--- printdata.py
+++ printdata.py
@@ -14,13 +14,23 @@ stmt = "SELECT latitude, longitude FROM gps_locations ORDER BY created_date DESC
 
 cursor.execute(stmt)
 row = cursor.fetchone()
-cursor.close()
-mariadb_connection.close()
+
 
 output_json = {
     "long":  float(row[0]),
-    "lat": float(row[1])
+    "lat": float(row[1]),
 }
+stmt_2 = "SELECT islocked FROM gyro_status;"
+cursor.execute(stmt_2)
+row = cursor.fetchone()
+
+output_json_2 = {
+    "islocked": round(float(row[0]))
+}
+
+cursor.close()
+mariadb_connection.close()
 
 print("Content-type: application/json\n")
 print(json.dumps(output_json))
+print(json.dumps(output_json_2))
